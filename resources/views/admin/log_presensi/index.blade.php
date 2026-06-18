d7ee8eba52d2a8b5b82c1819d6be430f7a054e80
agilimam <agilimamalhafizi04@gmail.com>
2026-06-19 05:20:11 +0700

    Ubah Teks Button

--- resources/views/admin/log_presensi/index.blade.php
+++ resources/views/admin/log_presensi/index.blade.php
@@ -52,7 +52,7 @@
                 
                 <button @click="openModal = true" class="w-full md:w-auto flex items-center justify-center gap-2 px-6 py-2.5 bg-emerald-600 hover:bg-emerald-700 rounded-xl text-white font-bold transition-all shadow-md shadow-emerald-500/10 text-xs uppercase tracking-wider">
                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>
-                    Export Laporan Resmi
+                    Export PDF
                 </button>
             </div>
 
